c9989702dc12d58b9ba478ca1c5da5d5562b106a
bryansteele <bryan@steele101>
2024-03-10 11:38:30 -0400

    *

---
+++ 14_9_Else, And, Or, Not/04_de_morgans_law.py
@@ -0,0 +1,3 @@
+# Which of the following expressions is equivalent to A or B?
+
+not((not A) and (not B))
---
+++ 14_9_Else, And, Or, Not/05_order_of_operations.py
@@ -0,0 +1,8 @@
+# What is the value of:
+
+A or not B and C
+
+if (A, B, C) = (False, True, True)?
+
+
+# False
